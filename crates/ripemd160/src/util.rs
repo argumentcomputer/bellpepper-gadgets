89aee5e85110fc65c4be050a9582d70122d7da5e
Saravanan Vijayakumaran <sarva.v@gmail.com>
2024-03-28 15:57:37 +0530

    Simpler implementations of ripemd160_d1/d2

--- crates/ripemd160/src/util.rs
+++ crates/ripemd160/src/util.rs
@@ -1,5 +1,5 @@
 use bellpepper::gadgets::uint32::UInt32;
-use bellpepper_core::{boolean::AllocatedBit, boolean::Boolean, ConstraintSystem, SynthesisError};
+use bellpepper_core::{boolean::Boolean, ConstraintSystem, SynthesisError};
 use ff::PrimeField;
 
 pub fn swap_byte_endianness(bits: &Vec<Boolean>) -> Vec<Boolean> {
@@ -20,223 +20,31 @@ pub fn uint32_rotl(a: UInt32, by: usize) -> UInt32 {
 
 fn ripemd160_d1<'a, Scalar, CS>(
     mut cs: CS,
-    a: &'a Boolean,
-    b: &'a Boolean,
-    c: &'a Boolean,
+    y: &'a Boolean,
+    x: &'a Boolean,
+    z: &'a Boolean,
 ) -> Result<Boolean, SynthesisError>
 where
     Scalar: PrimeField,
     CS: ConstraintSystem<Scalar>,
 {
-    let d1_value = match (a.get_value(), b.get_value(), c.get_value()) {
-        (Some(a), Some(b), Some(c)) => {
-            // (a and b) or (c and not(b))
-            Some((a & b) | (c & !b))
-        }
-        _ => None,
-    };
-
-    match (a, b, c) {
-        (&Boolean::Constant(_), &Boolean::Constant(_), &Boolean::Constant(_)) => {
-            // They're all constants, so we can just compute the value.
-
-            return Ok(Boolean::Constant(d1_value.expect("they're all constants")));
-        }
-        (&Boolean::Constant(false), b, c) => {
-            // If a is false,
-            // (a and b) or (c and not(b))
-            // equals
-            // (c and not(b))
-            return Boolean::and(cs, &b.not(), c);
-        }
-        (_a, &Boolean::Constant(false), c) => {
-            // If b is false,
-            // (a and b) or (c and not(b))
-            // equals
-            // (a and c)
-            return Ok(c.clone());
-        }
-        (a, b, &Boolean::Constant(false)) => {
-            // If c is false,
-            // (a and b) or (c and not(b))
-            // equals
-            // (a and b)
-            return Boolean::and(cs, a, b);
-        }
-        (a, b, &Boolean::Constant(true)) => {
-            // If c is true,
-            // (a and b) or (c and not(b))
-            // equals
-            // (a or not(b))
-            return Boolean::or(cs, a, &b.not());
-        }
-        (a, &Boolean::Constant(true), _c) => {
-            // If b is true,
-            // (a and b) or (c and not(b))
-            // equals
-            // (a and true) or (false)
-            // equals
-            // a
-            return Ok(a.clone());
-        }
-        (&Boolean::Constant(true), b, c) => {
-            // If a is true,
-            // (a and b) or (c and not(b))
-            // equals
-            // (b) xor (c) xor (b and c)
-            return Boolean::or(cs, b, c);
-        }
-        (
-            &Boolean::Is(_) | &Boolean::Not(_),
-            &Boolean::Is(_) | &Boolean::Not(_),
-            &Boolean::Is(_) | &Boolean::Not(_),
-        ) => {}
-    }
-
-    let d1 = cs.alloc(
-        || "d1",
-        || {
-            d1_value.ok_or(SynthesisError::AssignmentMissing).map(|v| {
-                if v {
-                    Scalar::ONE
-                } else {
-                    Scalar::ZERO
-                }
-            })
-        },
-    )?;
-
-    // (a and b) or (c and not(b)) = d1
-    // ab + c(1-b) = d1
-    // b(a - c) = d1 - c
-    // (c - a)b = c - d1
-
-    cs.enforce(
-        || "d1 computation",
-        |_| c.lc(CS::one(), Scalar::ONE) - &a.lc(CS::one(), Scalar::ONE),
-        |_| b.lc(CS::one(), Scalar::ONE),
-        |_| c.lc(CS::one(), Scalar::ONE) - d1,
-    );
-
-    Ok(AllocatedBit::alloc(cs.namespace(|| "d1_alloc"), d1_value)
-        .unwrap()
-        .into())
+    let tmp1 = Boolean::and(cs.namespace(|| "x AND y"), x, y)?;
+    let tmp2 = Boolean::and(cs.namespace(|| "(!x) AND z"), &x.not(), z)?;
+    Boolean::or(cs.namespace(|| "(x AND y) OR ((!x) AND z)"), &tmp1, &tmp2)
 }
 
 fn ripemd160_d2<'a, Scalar, CS>(
     mut cs: CS,
-    a: &'a Boolean,
-    b: &'a Boolean,
-    c: &'a Boolean,
+    x: &'a Boolean,
+    y: &'a Boolean,
+    z: &'a Boolean,
 ) -> Result<Boolean, SynthesisError>
 where
     Scalar: PrimeField,
     CS: ConstraintSystem<Scalar>,
 {
-    let d2_value = match (a.get_value(), b.get_value(), c.get_value()) {
-        (Some(a), Some(b), Some(c)) => {
-            // (a) xor (b or not(c))
-            Some((a) ^ (b | !c))
-        }
-        _ => None,
-    };
-
-    match (a, b, c) {
-        (&Boolean::Constant(_), &Boolean::Constant(_), &Boolean::Constant(_)) => {
-            // They're all constants, so we can just compute the value.
-
-            return Ok(Boolean::Constant(d2_value.expect("they're all constants")));
-        }
-        (&Boolean::Constant(false), b, c) => {
-            // If a is false,
-            // (a) xor (b or not(c))
-            // equals
-            // (b or not(c))
-            return Boolean::or(cs, b, &c.not());
-        }
-        (a, &Boolean::Constant(false), c) => {
-            // If b is false,
-            // (a) xor (b or not(c))
-            // equals
-            // (a xor not(c))
-            return Boolean::xor(cs, a, &c.not());
-        }
-        (a, _b, &Boolean::Constant(false)) => {
-            // If c is false,
-            // (a) xor (b or not(c))
-            // equals
-            // (a) xor (true)
-            // equals
-            // not a
-            return Ok(a.not());
-        }
-        (a, b, &Boolean::Constant(true)) => {
-            // If c is true,
-            // (a) xor (b or not(c))
-            // equals
-            // (a) xor (b)
-            return Boolean::xor(cs, a, b);
-        }
-        (a, &Boolean::Constant(true), _c) => {
-            // If b is true,
-            // (a) xor (b or not(c))
-            // equals
-            // (a) xor (true)
-            // equals
-            // not a
-            return Ok(a.not());
-        }
-        (&Boolean::Constant(true), b, c) => {
-            // If a is true,
-            // (a) xor (b or not(c))
-            // equals
-            // (not(b) and c)
-            return Boolean::and(cs, c, &b.not());
-        }
-        (
-            &Boolean::Is(_) | &Boolean::Not(_),
-            &Boolean::Is(_) | &Boolean::Not(_),
-            &Boolean::Is(_) | &Boolean::Not(_),
-        ) => {}
-    }
-
-    let d2 = cs.alloc(
-        || "d2",
-        || {
-            d2_value.ok_or(SynthesisError::AssignmentMissing).map(|v| {
-                if v {
-                    Scalar::ONE
-                } else {
-                    Scalar::ZERO
-                }
-            })
-        },
-    )?;
-
-    // (a) xor (b or not(c)) = d2
-    // (not a) * (b + (1 - c)) + (a) * (1 - b) * (c)
-    // 2ac - abc - ab - a - c + 1 + b = d2
-    // b * (a + ac) = 2ac - a - c + 1 + b - d2
-
-    let ac = Boolean::and(cs.namespace(|| "(a and c)"), a, c)?;
-
-    cs.enforce(
-        || "d2 computation",
-        |_| b.lc(CS::one(), Scalar::ONE),
-        |_| a.lc(CS::one(), Scalar::ONE) + &ac.lc(CS::one(), Scalar::ONE),
-        |lc| {
-            lc + CS::one() + &b.lc(CS::one(), Scalar::ONE)
-                - &c.lc(CS::one(), Scalar::ONE)
-                - &a.lc(CS::one(), Scalar::ONE)
-                + &ac.lc(CS::one(), Scalar::ONE)
-                + &ac.lc(CS::one(), Scalar::ONE)
-                - d2
-        },
-    );
-
-    Ok(AllocatedBit::alloc(cs.namespace(|| "d2_alloc"), d2_value)
-        .unwrap()
-        .into())
+    let tmp = Boolean::or(cs.namespace(|| "y OR !z"), y, &z.not())?;
+    Boolean::xor(cs.namespace(|| "x XOR (y OR !z)"), x, &tmp)
 }
 
 fn triop<Scalar, CS, U>(
